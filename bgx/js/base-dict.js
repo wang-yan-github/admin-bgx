02d956c89527493f3d396379d5253f80cc6e665a
wangyanjava <wangyanjava@gmail.com>
2018-05-14 09:44:28 +0800

    拉通数据前后台数据
    优化-基础信息管理-字典数据管理-修改删除类型提示信息
    优化-基础信息管理-图片广告管理-修改删除图片提示信息

--- bgx/js/base-dict.js
+++ bgx/js/base-dict.js
@@ -232,9 +232,15 @@ function deleteById(id) {
                 url: deleteAddress,
                 data: {"id": id},
                 success: function (data) {
-                    swal("操作成功!", "已成功删除数据！", "success");
-                    //重新加载数据
-                    $(".table").bootstrapTable('refresh', {url: loadAddress});
+                    if(data.code == 200){
+                        swal("操作成功!", "已成功删除数据！", "success");
+                        //重新加载数据
+                        $(".table").bootstrapTable('refresh', {url: loadAddress});
+                    }else{
+                        swal("操作成功!", data.msg, "success");
+                        //重新加载数据
+                        $(".table").bootstrapTable('refresh', {url: loadAddress});
+                    }
                 },
                 error: function (data) {
                     swal("操作异常，请刷新页面！", data.msg, "error");
